9ae9593dca10e562614511951a8ce3076705afd2
Prasath M <prasath@deforay.com>
2020-06-16 13:27:24 +0530

    Fixed some UI issues

--- resources/views/layoutsections/mainmenu.blade.php
+++ resources/views/layoutsections/mainmenu.blade.php
@@ -1,16 +1,9 @@
+
 <div class="header-navbar navbar-expand-sm navbar navbar-horizontal navbar-fixed navbar-dark navbar-without-dd-arrow navbar-shadow" role="navigation" data-menu="menu-wrapper">
-    <div class="navbar-container main-menu-content container center-layout" data-menu="menu-container">
+    <div class="navbar-container main-menu-content " data-menu="menu-container">
         <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="index.html" data-toggle="dropdown"><i class="la la-home"></i><span data-i18n="Dashboard">Dashboard</span></a>
-                <ul class="dropdown-menu">
-                    <li class="active" data-menu=""><a class="dropdown-item" href="dashboard-ecommerce.html" data-toggle=""><i class="la la-cart-plus"></i><span data-i18n="eCommerce">eCommerce</span></a>
+        <li  class="dropdown nav-item" id="dashboard" ><a class="dropdown-item single" href="/dashboard" data-toggle=""><i class="la la-home white"></i><span data-i18n="dashboard" class="white" >Dashboard</span></a>
                     </li>
-                    <li data-menu=""><a class="dropdown-item" href="dashboard-crypto.html" data-toggle=""><i class="la la-sitemap"></i><span data-i18n="Crypto">Crypto</span></a>
-                    </li>
-                    <li data-menu=""><a class="dropdown-item" href="dashboard-sales.html" data-toggle=""><i class="la la-dollar"></i><span data-i18n="Sales">Sales</span></a>
-                    </li>
-                </ul>
-            </li>
             <li class="dropdown nav-item" data-menu="dropdown"><a class="dropdown-toggle nav-link" href="#" data-toggle="dropdown"><i class="la la-television"></i><span data-i18n="Templates">Templates</span></a>
                 <ul class="dropdown-menu">
                     <li class="dropdown dropdown-submenu" data-menu="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown"><i class="la la-arrows-v"></i><span data-i18n="Vertical">Vertical</span></a>
