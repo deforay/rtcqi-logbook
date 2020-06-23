969feef0eb75e2c734485e46407e92374d8c29bc
Sudarmathi <sudarmathi@deforay.com>
2020-06-23 19:23:39 +0530

    missing query added

--- asm-pi.sql
+++ asm-pi.sql
@@ -552,3 +552,4 @@ ALTER TABLE `item_types` ADD `created_on` DATETIME NULL AFTER `item_type_status`
 ALTER TABLE `brands` CHANGE `manufaturer_name` `manufacturer_name` VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
 ALTER TABLE `brands` CHANGE `status` `brand_status` VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL;
 ALTER TABLE `brands` ADD `created_on` DATETIME NULL AFTER `brand_status`, ADD `updated_on` DATETIME NULL AFTER `created_on`, ADD `created_by` INT NULL AFTER `updated_on`, ADD `updated_by` INT NULL AFTER `created_by`;
+ALTER TABLE `items` ADD `created_on` DATETIME NULL AFTER `base_unit`, ADD `updated_on` DATETIME NULL AFTER `created_on`, ADD `created_by` INT NULL AFTER `updated_on`, ADD `updated_by` INT NULL AFTER `created_by`
