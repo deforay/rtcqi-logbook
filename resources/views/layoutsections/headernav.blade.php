92d23fdce139554c0ed6ac4306c70ff4bba8d70f
Sriram V <sriram@deforay.com>
2020-06-26 11:28:10 +0530

    Small issue fixed

--- resources/views/layoutsections/headernav.blade.php
+++ resources/views/layoutsections/headernav.blade.php
@@ -3,7 +3,7 @@
             <div class="navbar-header">
                 <ul class="nav navbar-nav flex-row">
                     <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
-                    <li class="nav-item"><a class="navbar-brand" href="index.html"><img class="brand-logo" alt="modern admin logo" src="{{ asset('app-assets/images/logo/asm_logo.png')}}">
+                    <li class="nav-item"><a class="navbar-brand" href="/dashboard"><img class="brand-logo" alt="modern admin logo" src="{{ asset('app-assets/images/logo/asm_logo.png')}}">
                             <h3 class="brand-text">ASM</h3>
                         </a></li>
                     <li class="nav-item d-md-none"><a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="la la-ellipsis-v"></i></a></li>
