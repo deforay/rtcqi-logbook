8e244d17dbb5f8c7ad56cc81bfbbf51323590ac8
Prasath M <prasath@deforay.com>
2020-07-08 17:43:44 +0530

    Quotes View

--- routes/web.php
+++ routes/web.php
@@ -138,6 +138,7 @@ Route::get('/quotes', 'Quotes\QuotesController@index')->name('quotes.index')->mi
 Route::post('/getAllQuotes', 'Quotes\QuotesController@getAllQuotes');
 Route::get('/quotes/edit/{id}', 'Quotes\QuotesController@edit')->middleware('access');
 Route::post('/quotes/edit/{id}', 'Quotes\QuotesController@edit');
+Route::post('/quoteDetailsView/{id}', 'Quotes\QuotesController@quoteDetailsView');
 
 
 //Mail template module
@@ -145,8 +146,8 @@ Route::get('/mailtemplate', 'MailTemplate\MailTemplateController@index')->name('
 Route::post('/previewMailTemplate/{id}/{saleType}', 'MailTemplate\MailTemplateController@previewMailTemplate');
 Route::post('/getTemplates', 'MailTemplate\MailTemplateController@getTemplates');
 Route::post('/mailtemplate/add', 'MailTemplate\MailTemplateController@add');
-Route::get('/mailtemplate/add', 'MailTemplate\MailTemplateController@add')->middleware('access');
-Route::get('/mailtemplate/edit/{id}', 'MailTemplate\MailTemplateController@edit')->middleware('access');
+Route::get('/mailtemplate/add', 'MailTemplate\MailTemplateController@add');
+Route::get('/mailtemplate/edit/{id}', 'MailTemplate\MailTemplateController@edit');
 Route::post('/mailtemplate/edit/{id}', 'MailTemplate\MailTemplateController@edit');
 Route::post('/addTemplate', 'MailTemplate\MailTemplateController@addTemplate');
 
