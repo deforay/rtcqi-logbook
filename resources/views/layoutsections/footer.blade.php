ab4716e6660c724c97ad7ef1c09eae50beaf9910
Amit Dugar <amit@deforay.com>
2020-07-23 15:45:27 +0530

    Removed Deforay linkfrom Footer

--- resources/views/layoutsections/footer.blade.php
+++ resources/views/layoutsections/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer footer-transparent footer-light navbar-shadow">
-    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2 container center-layout"><span class="float-md-left d-block d-md-inline-block">Copyright &copy; 2020 <a class="text-bold-800 grey darken-2" href="https://www.deforay.com/" target="_blank">Deforay </a>, All rights reserved.</span></p>
+    
 </footer>
