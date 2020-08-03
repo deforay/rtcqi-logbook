08bb295da8dca985af2df97864423ed158e51423
Sudarmathi <sudarmathi@deforay.com>
2020-08-03 12:14:54 +0530

    purchase order number added in delivery schedule

--- app/Model/DeliverySchedule/DeliveryScheduleTable.php
+++ app/Model/DeliverySchedule/DeliveryScheduleTable.php
@@ -52,11 +52,11 @@ class DeliveryScheduleTable extends Model
         if(session('loginType')=='users'){
             $data = DB::table('delivery_schedule')
                     ->join('items', 'items.item_id', '=', 'delivery_schedule.item_id')
-                    ->join('purchase_order_details', 'purchase_order_details.pod_id', '=', 'delivery_schedule.pod_id');
+                    ->join('purchase_order_details', 'purchase_order_details.pod_id', '=', 'delivery_schedule.pod_id')
+                    ->join('purchase_orders', 'purchase_orders.po_id', '=', 'purchase_order_details.po_id');
                     if(isset($req['poId']) && $req['poId'])
                        $data = $data->where('purchase_order_details.po_id', '=', $req['poId']);
                     // ->where('purchase_order_details.delivery_status', '=', 'pending')
-                    // ->join('purchase_orders', 'purchase_orders.po_id', '=', 'purchase_order_details.po_id')
             $data = $data->get();
         }
         else{
