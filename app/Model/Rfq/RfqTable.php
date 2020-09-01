c33bfe1efa6545a7e7654f976ace3ee4878b38b2
Sudarmathi <sudarmathi@deforay.com>
2020-09-01 17:26:06 +0530

    add quotes added only for vendors

--- app/Model/Rfq/RfqTable.php
+++ app/Model/Rfq/RfqTable.php
@@ -163,7 +163,7 @@ class RfqTable extends Model
         $userId=session('userId');
         if(session('loginType')=='users'){
             $data = DB::table('rfq')
-                    ->join('quotes', 'quotes.rfq_id', '=', 'rfq.rfq_id')
+                    // ->join('quotes', 'quotes.rfq_id', '=', 'rfq.rfq_id')
                     ->orderBy('rfq_issued_on', 'desc')
                     ->get();
         }
@@ -185,7 +185,7 @@ class RfqTable extends Model
         $userId=session('userId');
         if(session('loginType')=='users'){
             $data = DB::table('rfq')
-                ->join('quotes', 'quotes.rfq_id', '=', 'rfq.rfq_id')
+                // ->join('quotes', 'quotes.rfq_id', '=', 'rfq.rfq_id')
                 ->where('rfq_status','=','active')
                 ->orderBy('rfq_issued_on', 'desc')
                 ->get();
