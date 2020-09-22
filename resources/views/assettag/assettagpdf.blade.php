50ff1212930890cd967d14d59a05bfea753850ea
Sudarmathi <sudarmathi@deforay.com>
2020-09-22 19:36:12 +0530

    multiple label in single pdf done

--- resources/views/assettag/assettagpdf.blade.php
+++ resources/views/assettag/assettagpdf.blade.php
@@ -2,16 +2,68 @@
 <html>
 <head>
     <title></title>
+    <style media="screen"> 
+        table { 
+            border-style: dotted;
+            border-collapse: collapse; 
+        } 
+        table td { 
+            /* padding: 0.5rem; */
+            border-style: dotted;
+            /* border: 2px solid;  */
+        } 
+        table th { 
+            /* padding: 0.5rem;  */
+            border-style: dotted;
+            /* border: 2px solid;  */
+        } 
+    </style> 
 </head>
 <body>
-    <div class="container text-center">
-        <img src="{{$assetResult[0]->logo}}" alt="logo"><br/>
-        <h2 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h2>
-
-        <div class="" style="border: 1px solid #a1a1a1;padding: 15px;">
-            <div>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_tag, 'C128')!!}</div></br>
-        </div>
-        {{-- @php print_r($result); @endphp --}}
+    <div class="container">
+        <table style="width:100%">
+        <?php
+        for($i=0;$i<5;$i++){
+        ?>
+        <tr>
+            <td style="padding:6px;">
+                <img src="{{$assetResult[0]->logo}}" alt="logo">
+                <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_id, 'C128')!!}</span>
+            </td>
+            <td style="padding:6px;">
+                <img src="{{$assetResult[0]->logo}}" alt="logo">
+                <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_id, 'C128')!!}</span>
+            </td>
+            <td style="padding:6px;">
+                <img src="{{$assetResult[0]->logo}}" alt="logo">
+                <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_id, 'C128')!!}</span>
+            </td>
+        </tr>
+            {{-- <div style="clear:both;position: relative;margin-top:0.5%">
+                <div style="position:absolute; left:0pt;width:172pt;border: 1px solid;border-style: dotted;padding:4px;">
+                    <img src="{{$assetResult[0]->logo}}" alt="logo">
+                    <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                    <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_tag, 'C128')!!}</span>
+                </div>
+                <div style="position:absolute;margin-left:185pt;width:172pt;border: 1px solid;border-style: dotted;padding:4px;">
+                    <img src="{{$assetResult[0]->logo}}" alt="logo">
+                    <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                    <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_tag, 'C128')!!}</span>
+                </div>
+                <div style="margin-left:370pt;width:172pt;border: 1px solid;border-style: dotted;padding:4px;">
+                    <img src="{{$assetResult[0]->logo}}" alt="logo">
+                    <h3 style="text-transform:uppercase;">{{$assetResult[0]->asset_tag}}</h3>
+                    <span>{!!DNS1D::getBarcodeHTML($assetResult[0]->asset_tag, 'C128')!!}</span>
+                </div>
+            </div> --}}
+            {{-- <br/> --}}
+        <?php
+        }
+        ?>
+        </table>
     </div>
 </body>
 </html>
