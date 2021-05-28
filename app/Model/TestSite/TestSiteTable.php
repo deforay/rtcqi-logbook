f5859b2e3349493f14a1fe1b763db697e7e0d41a
Prasath M <prasath@deforay.com>
2021-05-28 12:10:54 +0530

    Test site is done

---
+++ app/Model/TestSite/TestSiteTable.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Model\TestSite;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+use App\Service\CommonService;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class TestSiteTable extends Model
+{
+    protected $table = 'test_site';
+
+    //add TestSite
+    public function saveTestSite($request)
+    {
+        //to get all request values
+        $data = $request->all();
+        $commonservice = new CommonService();
+        if ($request->input('siteName')!=null && trim($request->input('siteName')) != '') {
+            $id = DB::table('test_site')->insertGetId(
+                ['site_ID' => $data['siteId'],
+                'site_name' => $data['siteName'],
+                'site_latitude' => $data['latitude'],
+                'site_longitude' => $data['longitude'],
+                'site_address1' => $data['address1'],
+                'site_address2' => $data['address2'],
+                'site_postal_code' => $data['postalCode'],
+                'site_city' => $data['city'],
+                'site_state' => $data['state'],
+                'site_country' => $data['country'],
+                'test_site_status' => $data['testSiteStatus'],
+                'created_by' => session('userId'),
+                'created_on' => $commonservice->getDateTime(),
+                ]
+            );
+        }
+
+        return $id;
+    }
+
+    // Fetch All TestSite List
+    public function fetchAllTestSite()
+    {
+        $data = DB::table('test_site')
+                ->get();
+        return $data;
+    }
+
+    // Fetch All Active TestSite List
+    public function fetchAllActiveTestSite()
+    {
+        $data = DB::table('test_site')
+                ->where('test_site_status','=','active')
+                ->get();
+        return $data;
+    }
+
+     // fetch particular TestSite details
+     public function fetchTestSiteById($id)
+     {
+
+         $id = base64_decode($id);
+         $data = DB::table('test_site')
+                ->where('test_site.ts_id', '=',$id )
+                ->get();
+         return $data;
+     }
+
+     // Update particular TestSite details
+    public function updateTestSite($params,$id)
+    {
+        $commonservice = new CommonService();
+        $data = $params->all();
+            $testData = array(
+                'site_ID' => $data['siteId'],
+                'site_name' => $data['siteName'],
+                'site_latitude' => $data['latitude'],
+                'site_longitude' => $data['longitude'],
+                'site_address1' => $data['address1'],
+                'site_address2' => $data['address2'],
+                'site_postal_code' => $data['postalCode'],
+                'site_city' => $data['city'],
+                'site_state' => $data['state'],
+                'site_country' => $data['country'],
+                'test_site_status' => $data['testSiteStatus'],
+                'updated_by' => session('userId'),
+                'updated_on' => $commonservice->getDateTime()
+            );
+            $response = DB::table('test_site')
+                ->where('ts_id', '=',base64_decode($id))
+                ->update(
+                        $testData
+                    );
+        return $response;
+    }
+
+    
+}
---
+++ app/Service/TestSiteService.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Author             : Prasath M
+Date               : 28 May 2021
+Description        : Test Site Service Page
+Last Modified Date : 28 May 2021
+Last Modified Name : Prasath M
+*/
+
+namespace App\Service;
+use App\Model\TestSite\TestSiteTable;
+use DB;
+
+class TestSiteService
+{
+   
+    public function saveTestSite($request)
+    {
+    	$data =  $request->all();
+    	DB::beginTransaction();
+    	try {
+			$model = new TestSiteTable();
+        	$add = $model->saveTestSite($request);
+			if($add>0){
+                DB::commit();
+				$msg = 'TestSite Added Successfully';
+				return $msg;
+			}
+	    }
+	    catch (Exception $exc) {
+	    	DB::rollBack();
+	    	$exc->getMessage();
+	    }
+	}
+	
+	//Get All TestSite List
+	public function getAllTestSite()
+    {
+		$model = new TestSiteTable();
+        $result = $model->fetchAllTestSite();
+        return $result;
+	}
+
+	//Get All TestSite List
+	public function getAllActiveTestSite()
+    {
+		$model = new TestSiteTable();
+        $result = $model->fetchAllActiveTestSite();
+        return $result;
+	}
+	//Get Particular TestSite Details
+	public function getTestSiteById($id)
+	{
+		
+		$model = new TestSiteTable();
+        $result = $model->fetchTestSiteById($id);
+        return $result;
+	}
+	//Update Particular TestSite Details
+	public function updateTestSite($params,$id)
+    {
+    	DB::beginTransaction();
+    	try {
+			$model = new TestSiteTable();
+        	$add = $model->updateTestSite($params,$id);
+			if($add>0){
+                DB::commit();
+				$msg = 'TestSite Updated Successfully';
+				return $msg;
+			}
+	    }
+	    catch (Exception $exc) {
+	    	DB::rollBack();
+	    	$exc->getMessage();
+	    }
+	}
+	
+}
+
+?>
