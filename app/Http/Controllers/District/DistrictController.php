448ee4b4711c8bead024b1a6285a51048cfcf6c2
Prasath M <prasath@deforay.com>
2021-05-31 16:09:41 +0530

    District completed

---
+++ app/Http/Controllers/District/DistrictController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\District;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Service\DistrictService;
+use App\Service\ProvinceService;
+use Yajra\DataTables\Facades\DataTables;
+use Redirect;
+use Session;
+
+class DistrictController extends Controller
+{
+    //View District main screen
+    public function index()
+    {
+        if(session('login')==true)
+        {
+            return view('district.index');
+        }
+        else
+            return Redirect::to('login')->with('status', 'Please Login');
+    }
+
+    //Add District (display add screen and add the District values)
+    public function add(Request $request)
+    {
+        if ($request->isMethod('post')) 
+        {
+            $addDistrict = new DistrictService();
+            $result = $addDistrict->saveDistrict($request);
+            return Redirect::route('district.index')->with('status', $result);
+        }
+        else
+        {
+            $ProvinceService = new ProvinceService();
+            $province = $ProvinceService->getAllActiveProvince();   
+            return view('district.add',array('province'=>$province));
+        }
+    }
+
+    // Get all the  District list
+    public function getAllDistrict(Request $request)
+    {
+        $service = new DistrictService();
+        $data = $service->getAllDistrict();
+        return DataTables::of($data)
+                    ->addColumn('action', function($data){
+                        $button = '<div>';
+                        $button .= '<a href="/district/edit/'. base64_encode($data->district_id).'" name="edit" id="'.$data->district_id.'" class="btn btn-outline-primary btn-sm" title="Edit"><i class="ft-edit"></i></a>';
+                        $button .= '</div>';
+                        return $button;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+    }
+
+    //edit District
+    public function edit($id,Request $request)
+    {
+        if ($request->isMethod('post')) 
+        {
+            $DistrictService = new DistrictService();
+            $edit = $DistrictService->updateDistrict($request,$id);
+            return Redirect::route('district.index')->with('status', $edit);
+        }
+        else
+        {
+            $ProvinceService = new ProvinceService();
+            $province = $ProvinceService->getAllActiveProvince();
+            $DistrictService = new DistrictService();
+            $result = $DistrictService->getDistrictById($id);
+            return view('district.edit',array('result'=>$result,'province'=>$province,'id'=>$id));
+        }
+    }
+}
+
