0f8d2cf18a5310efc95489f0e72e46673a134449
Prasath M <prasath@deforay.com>
2021-06-03 15:47:47 +0530

    Completed Global config

---
+++ app/Service/GlobalConfigService.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Author             : Prasath M
+Date               : 03 Jun 2021
+Description        : Global Config Service Page
+Last Modified Date : 03 Jun 2021
+Last Modified Name : Prasath M
+*/
+
+namespace App\Service;
+use App\Model\GlobalConfig\GlobalConfigTable;
+use DB;
+
+class GlobalConfigService
+{
+   
+	//Get All GlobalConfig List
+	public function getAllGlobalConfig()
+    {
+		$model = new GlobalConfigTable();
+        $result = $model->fetchAllGlobalConfig();
+        return $result;
+	}
+
+	//Update Particular GlobalConfig Details
+	public function updateGlobalConfig($params)
+    {
+    	DB::beginTransaction();
+    	try {
+			$model = new GlobalConfigTable();
+        	$add = $model->updateGlobalConfig($params);
+			if($add>0){
+                DB::commit();
+				$msg = 'GlobalConfig Updated Successfully';
+				return $msg;
+			}
+	    }
+	    catch (Exception $exc) {
+	    	DB::rollBack();
+	    	$exc->getMessage();
+	    }
+	}
+	
+}
+
+?>
