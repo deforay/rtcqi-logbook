33a6cb22d30311a50ef7fb2a12d72407cdca2eda
Sivakumar <sivakumar@deforay.com>
2021-06-07 15:36:26 +0530

    To add a new screen for trend report

---
+++ app/Http/Controllers/TrendReport/TrendReportController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\TrendReport;
+
+use App\Http\Controllers\Controller;
+use App\Service\TestSiteService;
+use App\Service\MonthlyReportService;
+use App\Service\FacilityService;
+use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
+use Redirect;
+use Session;
+
+class TrendReportController extends Controller
+{
+    //View Trend Report main screen
+    public function index()
+    {
+        if(session('login')==true)
+        {
+            $FacilityService = new FacilityService();
+            $facility = $FacilityService->getAllActiveFacility();
+            $TestSiteService = new TestSiteService();
+            $testSite = $TestSiteService->getAllActiveTestSite();
+            $monthlyReportService = new MonthlyReportService();
+            $monthlyReport = $monthlyReportService->getAllActiveMonthlyReport();
+            return view('trendreport.index',array('testSite'=>$testSite,'facility'=>$facility,'monthlyReport'=>$monthlyReport));
+        }
+        else
+            return Redirect::to('login')->with('status', 'Please Login');
+    }
+
+    public function getTrendMonthlyReport(Request $request)
+    {
+        // dd($request);die;
+        $monthlyReportService = new MonthlyReportService();
+        $data = $monthlyReportService->getTrendMonthlyReport($request);
+        return DataTables::of($data)
+                    ->make(true);
+    }
+}
+
