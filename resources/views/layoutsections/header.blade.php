91432ac20d6a5a4a9f4ed7fb01fffb6f664a394f
Sakthivel <sakthivel.p@deforay.com>
2021-08-20 14:31:50 +0530

    To remove the unwanted js and css files

--- resources/views/layoutsections/header.blade.php
+++ resources/views/layoutsections/header.blade.php
@@ -48,7 +48,6 @@
 <link rel="stylesheet" type="text/css" href="{{asset('app-assets/css/core/colors/palette-switch.min.css')}}">
 
 <!-- BEGIN: Custom CSS-->
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css')}}">
 <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
