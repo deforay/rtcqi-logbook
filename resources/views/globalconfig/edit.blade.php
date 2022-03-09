8ecffa5c4a3e06195183c13b1a38a91545179efe
Sakthivel <sakthivel.p@deforay.com>
2022-03-09 18:17:08 +0530

    Small changes on global config edit page

--- resources/views/globalconfig/edit.blade.php
+++ resources/views/globalconfig/edit.blade.php
@@ -11,7 +11,7 @@
 <div class="content-wrapper">
     <div class="content-header row">
         <div class="content-header-left col-md-10 col-12 mb-2 breadcrumb-new">
-            <h3 class="content-header-title mb-0 d-inline-block">Province</h3>
+            <h3 class="content-header-title mb-0 d-inline-block">Global Config</h3>
             <div class="row breadcrumbs-top d-inline-block">
                 <div class="breadcrumb-wrapper col-12">
                     <ol class="breadcrumb">
