9adaf2b22e2bc90605d46e5c4a0257a0b8f74fda
Sakthi Kala <sakthi@deforay.com>
2024-05-17 16:15:15 +0530

    send mail cron function changes

--- app/Console/Commands/SendMailCron.php
+++ app/Console/Commands/SendMailCron.php
@@ -42,32 +42,38 @@ class SendMailCron extends Command
     {
         $commonservice = new CommonService();
         $tempMail = DB::table('temp_mail')
-                    ->where('status','=', 'pending')
-                    ->offset(0) // Starting position of records
-                    ->limit(10)
-                    ->get()->toArray();
+            ->where('status', '=', 'pending')
+            ->offset(0) // Starting position of records
+            ->limit(10)
+            ->get()->toArray();
+    
         if (count($tempMail) > 0) {
-            foreach($tempMail as $mail){
-                //Update mail status
-                $commonservice->updateTempMailStatus($mail->temp_id,'not-sent');
-                
+            foreach ($tempMail as $mail) {
+                // Update mail status
+                $commonservice->updateTempMailStatus($mail->temp_id, 'not-sent');
+    
                 $msg['msg'] =  $mail->message;
-                Mail::send('email.emailCron', $msg, function ($message) use ($mail) {
-                    $send = $message->to($mail->to_email, $mail->from_full_name)
-                                    ->subject($mail->subject);
-                            
-                });
-
-                if (Mail::failures()) {
-                    \Log::info('Mail sent failed for - '.$mail->temp_id);
+                if (isset($mail->to_email) && $mail->to_email != '') {
+                    try {
+                        Mail::send('email.emailCron', $msg, function ($message) use ($mail) {
+                            $toMail = explode(',', $mail->to_email);
+                            $message->to($toMail[0], $mail->from_full_name)
+                                ->subject($mail->subject);
+                            unset($toMail[0]);
+                            foreach ($toMail as $cc) {
+                                $message->cc(trim($cc));
+                            }
+                        });
+    
+                        // If no exception, consider the email sent successfully
+                        $commonservice->updateTempMailStatus($mail->temp_id, 'sent');
+                        $commonservice->deleteTempMail($mail->temp_id);
+                    } catch (\Exception $e) {
+                        \Log::error('Mail sent failed for - ' . $mail->temp_id . '. Error: ' . $e->getMessage());
+                    }
                 }
-                else {
-                    //\Log::info('Mail sent successfully - '.$mail->temp_id.'to '.$mail->customer_name);
-                    //Delete mail details
-                    $commonservice->updateTempMailStatus($mail->temp_id,'sent');
-                    $commonservice->deleteTempMail($mail->temp_id);
-                }
-            } 
+            }
         }
     }
+    
 }
