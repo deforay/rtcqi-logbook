fddc06b83c208697329fa682217e601f80e0f23b
Sijulda <sijulda"deforay.com>
2024-05-24 07:54:12 +0530

    email template changes

--- resources/views/email/forgetPassword.blade.php
+++ resources/views/email/forgetPassword.blade.php
@@ -1,4 +1,13 @@
 <h1>Reset Password</h1>
    
-We have received a request to reset your password. Please click the below link to reset it
-<a href="{{ route('reset.password', $token) }}">Reset Password</a>
+You or someone using your login id, requested for password reset on <span style='font-weight:bold;'>{{env('APP_URL')}}</span>.
+<div style="margin-top:10px;">
+    Please click the link or copy and paste in the browser
+</div>
+<div style="margin-top:10px; margin-bottom:30px">
+    <a href="{{ env("APP_URL").'/reset-password/'.$token }}">{{ env("APP_URL").'/reset-password/'.$token }}</a>
+</div>
+<hr/>
+<div style="margin-top:10px;">
+    Please note this is a system generated email
+</div>
