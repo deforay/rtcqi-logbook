7e20c6cd1c75b62591537ec8a97ffc1b87bbd05c
Sakthi Kala <sakthi@deforay.com>
2024-06-21 12:23:58 +0530

    locale issue fixes

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -10,12 +10,15 @@ class SetLocale
 {
     public function handle($request, Closure $next)
     {
-        $user_id = session('userId') ?? 1;
-        // dd($user_id);
-        $user = DB::table('users')->where('user_id', $user_id)->first();
-        $locale = session('locale', $user->prefered_language);
-        App::setLocale($locale);
+        $user_id = session('userId');
+        if($user_id != '' && $user_id != null) {
+            $user = DB::table('users')->where('user_id', $user_id)->first();
+            $locale = session('locale', $user->prefered_language);
+            App::setLocale($locale);
 
+            return $next($request);
+        }
+        App::setLocale("en");
         return $next($request);
     }
 }
